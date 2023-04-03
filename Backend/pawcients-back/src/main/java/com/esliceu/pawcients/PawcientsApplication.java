bda6aa6a3964a73fb6c5c581de73f3a47c1e2d09
MrAndrewX <apantojaa2002@gmail.com>
2023-04-03 12:26:44 +0200

    Fit: Added directory structure Backend - Data - Forntent

---
+++ Backend/pawcients-back/src/main/java/com/esliceu/pawcients/PawcientsApplication.java
@@ -0,0 +1,13 @@
+package com.esliceu.pawcients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawcientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawcientsApplication.class, args);
+	}
+
+}
