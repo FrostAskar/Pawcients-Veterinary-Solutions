8f8489b668c457cb5b9d7e1dea284de8b1327be9
Maru Suarez <marusuarezpastene@gmail.com>
2023-05-25 08:51:42 +0200

    feat: dashboard vet principal page

---
+++ Frontend/pawcients-front/src/Routes/Worker/TodayPatient.js
@@ -0,0 +1,31 @@
+import "css/vet/patientCard.scss";
+
+export default function TodayPatient(props) {
+    function getAppointmentType () {
+        if(props.patient.type == "Vaccine") {
+            return "vaccines"
+        } else if(props.patient.type == "Checkup") {
+            return "medical_services"
+        } else if(props.patient.type == "Surgery") {
+            return "monitor_heart";
+        } else if(props.patient.type == "Healing") {
+            return "healing"
+        }
+    }
+    return (
+            <div className="patient-card-wrapper">
+                <i id="type-icon" className="material-icons"> { getAppointmentType() }</i>
+                <div className="appoint">
+                    <p className="patient-hour">{props.patient.hour}</p>
+                    <p className="patient-type">{props.patient.type}</p>
+                </div>
+                <div className="patient">
+                    <p className="patient-name">{props.patient.name}</p>
+                    <p className="patient-owner">{props.patient.owner}</p>
+                </div>
+                <button id="status" className={`patient-status ${props.patient.active ? 'active-status' : 'completed-status'}`}>{ props.patient.active ? 'On work' : 'Completed'}</button>
+            </div>
+        
+    )
+}
+
--- Frontend/pawcients-front/src/Routes/Worker/VetDashboard.js
+++ Frontend/pawcients-front/src/Routes/Worker/VetDashboard.js
@@ -3,7 +3,8 @@ import "css/global/variables.css";
 import "css/vet/dashboard.scss";
 import Logo from "media/paw.png";
 import Profile from "media/vet.png";
-import PatientCard from "Routes/Worker/PatientCard";
+import Paw from "media/paw.png";
+import TodayPatient from "Routes/Worker/TodayPatient";
 import { Link } from "react-router-dom";
 import { fetchProfile } from "fetches/getProfile";
 
@@ -19,6 +20,8 @@ const todayPatients = [
     id: 1,
     name: "Laika",
     kind: "Dog",
+    hour: "9:30",
+    type: "Checkup",
     owner: "Maru Suarez",
     active: true,
   },
@@ -26,6 +29,8 @@ const todayPatients = [
     id: 2,
     name: "Luna",
     kind: "Dog",
+    hour: "10:30",
+    type: "Vaccine",
     owner: "Dámaso Simal",
     active: true,
   },
@@ -33,6 +38,8 @@ const todayPatients = [
     id: 3,
     name: "Ander",
     kind: "Cat",
+    hour: "11:30",
+    type: "Surgery",
     owner: "Andrés Pantoja",
     active: false,
   },
@@ -112,22 +119,75 @@ export default function VetDashboard() {
         </div>
 
         <div className="dashboard-content">
-          <div className="today-patients">
-            <div className="today-patients-header">
-              <h2> Today Patients </h2>
-            </div>
-            <div className="today-patients-body">
-              <div class="card-info">
-                <p className="patient-name">Pet name</p>
-                <p className="patient-kind">Kind</p>
-                <p className="patient-owner">Owner</p>
-                <p className="patient-status">Status</p>
+          <section className="row-dashboard">
+            <div className="modal">
+              <div className="modal-content">
+                <div className="total-patients">
+                  <div className="total-patients-row">
+                    <img src={Paw} alt="paw" height="50px" width="auto" />
+                    <div className="total-patients-text">
+                      <p>Total patients: </p>
+                      <p className="total-patients-number">214</p>
+                    </div>
+                  </div>
+                  <p>Today: <span className="today-patients-number"> 32 </span></p>
+                </div>
               </div>
-              {todayPatients.map((patient, index) => (
-                <PatientCard key={index} patient={patient} />
-              ))}
             </div>
-          </div>
+            <div className="modal">
+              <div className="modal-content">
+                <div className="patients-type">
+                  <h3>Patients by type</h3>
+                  <div className="patients-type-class">
+                    <div className="blue-circle"> </div>
+                    <p>Dog</p>
+                  </div>
+                  <div className="patients-type-class">
+                    <div className="red-circle"></div>
+                    <p>Cat</p>
+                  </div>
+                  <div className="patients-type-class">
+                    <div className="yellow-circle"></div>
+                    <p>Others</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="add-patient">
+              <button class="add-patient-button">Add patient</button>
+            </div>
+          </section>
+          <section class="row-dashboard">
+            <div className="modal">
+              <div className="modal-content">
+                <div className="today-patients">
+                  <div className="today-patients-header">
+                    <h2> Today Patients </h2>
+                    <select>
+                      <option value="All Vets"> All vets </option>
+                    </select>
+                  </div>
+                  <div className="today-patients-body">
+                    {todayPatients.map((patient, index) => (
+                      <TodayPatient key={index} patient={patient} />
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className="row-dashboard">
+            <div className="modal">
+              <div className="modal-content">
+                <div className="clinic-revenue">
+                  <h2>Clinic Revenue</h2>
+                  <h3>Gráfico to guapo</h3>
+                </div>
+              </div>
+            </div>
+          </section>
+
+
         </div>
       </div>
     </div>
