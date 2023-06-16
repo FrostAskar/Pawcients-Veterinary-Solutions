2746fc4b1ccdb28c36869f4e48df496c2a11ba7a
frost <damaso116simal@gmail.com>
2023-06-16 21:19:52 +0200

    Fix: Removed Throw when clients = 0

--- Frontend/pawcients-front/src/fetches/Global/FetchLogin.js
+++ Frontend/pawcients-front/src/fetches/Global/FetchLogin.js
@@ -1,11 +1,13 @@
 export async function fetchLogin(email, password) {
-  const response = await fetch("http://127.0.0.1:8080/login", {
+  const fetchUrl = process.env.REACT_APP_API_ENDPOINT;
+  const response = await fetch(`http://127.0.0.1:8080/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({ email, password }),
   });
+  console.log(fetchUrl);
   const data = await response.json();
 
   localStorage.setItem("token", data.token);
